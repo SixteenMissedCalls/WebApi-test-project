33ccc5e05c50e996d534da785e572828b3231000
Данил Насибуллин <ddnasibullin@gmail.com>
2025-06-29 11:11:05 +0300

    #Task-001 + init logger

--- Src/CurrencyRateGateway/Program.cs
+++ Src/CurrencyRateGateway/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace CurrencyRateGateway
 {
@@ -13,11 +14,18 @@ namespace CurrencyRateGateway
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine($"BUILD_CONFIGURATION - Конфигурация сборки: {Environment.GetEnvironmentVariable("BUILD_CONFIGURATION")}");
+            Console.WriteLine($"ASPNETCORE_ENVIRONMENT - Среда выполнения: {Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}");
+            
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((hostingContext, loggerConfig) => 
+                {
+                    loggerConfig.ReadFrom.Configuration(hostingContext.Configuration);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
